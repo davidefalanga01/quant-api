b1969f8b40a866237c1f17d81e988b8335fd07fa
davidefalanga01 <falangad01@gmail.com>
2025-03-25 18:06:38 +0100

    Created the initial api

--- api.py
+++ api.py
@@ -1,5 +1,26 @@
-import numpy as np
+from fastapi import FastAPI
+import yfinance as yf 
 
-x = [1, 2, 3]
+app = FastAPI()
 
+@app.get("/data/ticker/{symbol}")
+def get_stock_data(symbol: str, period: str = '1mo', frequency: str = "1d"):
+    """
+    Get historical markerìt data for a given ticker symbol.
+    :param symbol: Stock ticker symbol
+    :param period: Data period (e.g., 1d, 1m, 1y)
+    :param frequency: Sampling frequency of data (e.g., 1d, 1m, 3m)
+    """
 
+    try:
+        stock = yf.Ticker(symbol)
+        data = stock.history(period=period, interval=frequency) # return a DataFrame
+        indicators = calculate_indicators(data)
+
+        return indicators.to_dict()
+    except Exception as e:
+        return {'Error': str(e)}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8080)
---
+++ financial_indicators.py
@@ -0,0 +1,6 @@
+import pandas as pd 
+import numpy as np 
+
+# RSI
+# MACD
+# Bande di Bollingher
